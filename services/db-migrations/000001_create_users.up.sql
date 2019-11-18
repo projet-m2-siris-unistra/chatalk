b4a41234dc0a81121182d6af775e71f33a7244a4
Gabriel Poittevin <gabriel.poittevin@gmail.com>
2019-11-18 16:27:32 +0100

    Corrected migrations

--- services/db-migrations/000001_create_users.up.sql
+++ services/db-migrations/000001_create_users.up.sql
@@ -2,8 +2,8 @@ CREATE TABLE IF NOT EXISTS "users" (
   "user_id"       SERIAL NOT NULL PRIMARY KEY,
   "username"      VARCHAR(255) NOT NULL UNIQUE CHECK ("username" ~ '^[a-zA-Z0-9]+$'),
   "email"         VARCHAR(255) NOT NULL UNIQUE CHECK ("email" ~ '^.+@.+$'),
-  "password"      VARCHAR(255) NOT NULL,
+  "pw_hash"       VARCHAR(255) NOT NULL,
   "display_name"  VARCHAR(255),
   "status"        VARCHAR(512),
-  "avatar"        VARCHAR(64)
+  "pic_url"       VARCHAR(64)
 );
