2e7fdb11472e8cf7d87bf4ea3dbfd9a067d960cc
ludovicm67 <ludovicmuller1@gmail.com>
2019-11-26 13:41:06 +0100

    ui: add some commented auth logic

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -13,18 +13,28 @@ import Privacy from './component/Privacy';
 import Terms from './component/Terms';
 import WebsocketProvider from './component/WebsocketProvider';
 
+// import { State } from './store/state';
+// import { useSelector } from 'react-redux';
 
 const App: React.FC = () => {
+  // const auth = useSelector((state: State) => state.auth);
+
   return (
     <Provider store={store}>
-      <WebsocketProvider wsUrl="wss://ws.chatalk.fr">
+      <WebsocketProvider wsUrl="ws://localhost:42042">
         <CssBaseline/>
         <Router>
           <Switch>
             <Route exact path="/" component={ Homepage } />
-            <Route exact path="/login" component={ Login } />
-            <Route exact path="/register" component={ SignUp } />
-            <Route path="/conversation/:id?" component={ ConversationsList } />
+            {/* { !auth && (
+              <> */}
+                <Route exact path="/login" component={ Login } />
+                <Route exact path="/register" component={ SignUp } />
+              {/* </>
+            )}
+            { auth && */}
+              <Route path="/conversation/:id?" component={ ConversationsList } />
+              {/* } */}
             <Route exact path="/credits" component={ Credits } />
             <Route exact path="/privacy" component= { Privacy } />
             <Route exact path="/terms" component= { Terms } />
